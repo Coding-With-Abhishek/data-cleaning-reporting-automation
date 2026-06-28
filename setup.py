39b6450333f87482b760a606623efeaac87729b8
Coding-With-Abhishek <itsabhishek0008@gmail.com>
2026-06-28 08:06:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="data-cleaning-reporting-automation",
     version="1.0.0",
-    author="Abhishek Rajput",
+    author="Abhishek Kumar",
     description="Automated Data Cleaning and Reporting System using Python",
     packages=find_packages(),
     install_requires=[
